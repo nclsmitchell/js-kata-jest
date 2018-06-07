c352337007a70a2f864f0d1e2840e07a151d5ad9
nclsmitchell <ncls.mitchell@gmail.com>
2018-06-07 15:11:48 +0200

    handle multiple elements + tests

--- src/kata.js
+++ src/kata.js
@@ -1,6 +1,11 @@
 function add(string) {
   if (string.length > 0) {
-    return parseInt(string);
+    let sum = 0
+    const list = string.split(',');
+    for (let i=0; i<list.length; i++) {
+      sum += parseInt(list[i]);
+    }
+    return sum;
   }
   return 0;
 }
--- test/kata.test.js
+++ test/kata.test.js
@@ -7,3 +7,11 @@ test('return 0 if empty string provided', () => {
 test('return single provided element', () => {
   expect(add('1')).toBe(1);
 });
+
+test('return expected sum', () => {
+  expect(add('1, 2, 3')).toBe(6);
+});
+
+test('return expected sum with negative int provided', () => {
+  expect(add('1, 2, -4')).toBe(-1);
+});
