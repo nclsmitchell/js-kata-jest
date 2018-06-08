1c339f3a60a66e21bea570ec23b6be3c738b81cf
nclsmitchell <ncls.mitchell@gmail.com>
2018-06-08 13:03:14 +0200

    handle custom separators + custom rules

--- src/kata.js
+++ src/kata.js
@@ -1,10 +1,27 @@
-const separator = ',';
+const separators = [',','\\n'];
+const negatives = [];
 
 function add(string) {
+
+  if (string.match(/^\/\/(\D+)\n/)) {
+    separators.push(string.match(/^\/\/(\D+)\n/)[1]);
+  }
+  const regex = new RegExp(`[${separators.join('')}]`);
+
   if (string.length > 0) {
-    const sum = string.split(separator).reduce((acum, item) => {
-      return parseInt(acum) + parseInt(item);
-    })
+    const sum = string.split(regex)
+      .filter(item => !isNaN(parseInt(item)) && parseInt(item) < 1000)
+      .reduce((acum, item) => {
+        if (parseInt(item) >= 0) {
+          return parseInt(acum) + parseInt(item);
+        }
+        else {
+          negatives.push(parseInt(item));
+        }
+      })
+    if (negatives.length > 0) {
+      throw `Negative numbers are not allowed: ${negatives.join(', ')}`;;
+    }
     return parseInt(sum);
   }
   return 0;
--- test/kata.test.js
+++ test/kata.test.js
@@ -1,17 +1,33 @@
 const add = require('../src/kata');
 
-test('return 0 if empty string provided', () => {
+test('should return 0 if empty string provided', () => {
   expect(add('')).toBe(0);
 });
 
-test('return single provided element', () => {
+test('should return single provided element', () => {
   expect(add('1')).toBe(1);
 });
 
-test('return expected sum', () => {
+test('should return expected sum', () => {
   expect(add('1, 2, 3')).toBe(6);
 });
 
-test('return expected sum with negative int provided', () => {
-  expect(add('1, 2, -4')).toBe(-1);
+test('should accept \n as separator', () => {
+  expect(add('1\n2,3')).toBe(6);
+});
+
+test('should accept a custom separator', () => {
+  expect(add('//;\n1,2;3')).toBe(6);
+});
+
+test('should ignore above 1000 provided int', () => {
+  expect(add('2, 1001')).toBe(2);
+});
+
+test('should accept any length separator', () => {
+  expect(add('//***\n2***3***4')).toBe(9);
+});
+
+test('should throw negative provided int', () => {
+  expect(add('2,-5,-10,9')).toThrow('Negative numbers are not allowed: -5, -10');
 });
