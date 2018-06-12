f9f814724f4c7f729f63d7ec2630982e2f4b980f
nclsmitchell <ncls.mitchell@gmail.com>
2018-06-12 12:43:28 +0200

    refactor regexp

--- src/kata.js
+++ src/kata.js
@@ -2,34 +2,37 @@ function add(string) {
 
   const separators = [',','\\n'];
   const negatives = [];
+  const regexp = /^\/\/(\D+)\n/;
 
-  if (string.length > 0) {
-
-    if (string.match(/^\/\/(\D+)\n/)) {
-      separators.push(string.match(/^\/\/(\D+)\n/)[1]);
-    }
-    const regex = new RegExp(`[${separators.join('')}]`);
-
-    const sum = string.split(regex)
-      .filter(item => !isNaN(parseInt(item)) && parseInt(item) < 1000)
-      .reduce((acum, item) => {
-        if (parseInt(acum) < 0) {
-          negatives.push(parseInt(acum));
-        }
-        else if (parseInt(item) < 0) {
-          negatives.push(parseInt(item));
-        }
-        else {
-          return parseInt(acum) + parseInt(item);
-        }
-      })
-
-    if (negatives.length > 0) {
-      throw `Negative numbers are not allowed: ${negatives.join(', ')}`;
-    }
-    return parseInt(sum);
+  if (string.length == 0) {
+    return 0;
   }
-  return 0;
+
+  if (string.match(regexp)) {
+    separators.push(string.match(regexp)[1]);
+  }
+  const regex = new RegExp(`[${separators.join('')}]`);
+
+  const sum = string.split(regex)
+    .filter(item => !isNaN(parseInt(item)) && parseInt(item) < 1000)
+    .reduce((acum, item) => {
+      const parsedItem = parseInt(item)
+      const parsedAcum = parseInt(acum)
+      if (parsedAcum < 0) {
+        negatives.push(parsedAcum);
+      }
+      else if (parsedItem < 0) {
+        negatives.push(parsedItem);
+      }
+      else {
+        return parsedAcum + parsedItem;
+      }
+    })
+
+  if (negatives.length > 0) {
+    throw `Negative numbers are not allowed: ${negatives.join(', ')}`;
+  }
+  return parseInt(sum);
 }
 
 module.exports = add;
