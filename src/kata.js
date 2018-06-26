efe7377fa8d6ea35e09a2e86daa608ac1013872c
nclsmitchell <ncls.mitchell@gmail.com>
2018-06-26 10:54:44 +0200

    rename variables

--- src/kata.js
+++ src/kata.js
@@ -2,27 +2,28 @@ function add(string) {
 
   const separators = [',','\\n'];
   const negatives = [];
-  const regexp = /\/\/(\D)\n/;
-  const regexp_2 = /\[(.*?)\]/g;
+  const single_separator = /\/\/(\D)\n/;
+  const multiple_separator = /\[(.*?)\]/g;
+  const limit = 1000;
 
   if (string.length == 0) {
     return 0;
   }
 
-  if (string.match(regexp)) {
-    separators.push(string.match(regexp)[1]);
+  if (string.match(single_separator)) {
+    separators.push(string.match(single_separator)[1]);
   }
 
-  let match_2 = regexp_2.exec(string);
-  if (match_2) {
-    while (match_2 != null) {
-        separators.push(match_2[1]);
-        match_2 = regexp_2.exec(string);
+  let match = multiple_separator.exec(string);
+  if (match) {
+    while (match != null) {
+        separators.push(match[1]);
+        match = multiple_separator.exec(string);
      }
   }
 
   const list = string.split(new RegExp(`[${separators.join('')}]`))
-    .filter(item => !isNaN(parseInt(item)) && parseInt(item) < 1000)
+    .filter(item => !isNaN(parseInt(item)) && parseInt(item) < limit)
 
   let sum = 0;
   for (let i=0; i<list.length; i++) {
